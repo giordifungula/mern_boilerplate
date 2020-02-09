2e058bc4327ed57af56739cb6a46e0f9f637f6c1
giordifungula <jodi.fungula@gmail.com>
2020-02-09 21:37:35 +0200

    added the auth file to server

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ const express =  require('express')
 
 const app = express()
 
-app.get('/', (req,res)=> {
-    console.log("Testing the data")
-    res.json({
-        data: 'You git the base endpoint'
-    })
-})
+//imports here
+const authRoutes = require('./routes/auth')
+
 
 const port = process.env.port || 5000
+
 app.listen(port, ()=> {
+
     console.log(`We are running on port ${port}`)
+
 })
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,3 +1,12 @@
 const express =  require('express')
 
-const app = express()
+const router = express().router()
+
+router.get('/', (req,res) => {
+    console.log("Testing the data")
+    res.json({
+        data: 'You git the base endpoint'
+    })
+})
+
+module.exports = router
